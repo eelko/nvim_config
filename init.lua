67d0e863abeae3a01663e9f953f7c572879839c3
Eelko van Breda <eelko.vanbreda@evondos.com>
2024-08-19 16:06:34 +0200

    added scrollbar

--- init.lua
+++ init.lua
@@ -914,6 +914,8 @@ require('lazy').setup({
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
+  -- Scrollbar
+  { 'dstein64/nvim-scrollview' },
   -- { -- Collection of various small independent plugins/modules
   --   'echasnovski/mini.nvim',
   --   config = function()
