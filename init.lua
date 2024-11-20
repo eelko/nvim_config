9348eebeb389ac36fb9a704c934ca9ba9f66c6e9
Eelko van Breda <eelko.vanbreda@evondos.com>
2024-11-20 12:59:48 +0100

    added scroll animation

--- init.lua
+++ init.lua
@@ -721,7 +721,12 @@ require('lazy').setup({
       },
     },
   },
-
+  {
+    'karb94/neoscroll.nvim',
+    config = function()
+      require('neoscroll').setup {}
+    end,
+  },
   -- maximise vim windows
   {
     'szw/vim-maximizer',
